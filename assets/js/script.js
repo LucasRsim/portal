825fcc8331d99e6b5c7fdc11c565031d4b99a31b
LucasRsim <lucasnrsim@gmail.com>
2025-04-09 16:49:35 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
         checkboxes.forEach(checkbox => {
             checkbox.checked = false;
         });
-
     });
 
     document.getElementById('login-form').addEventListener('submit', async function(event) {
@@ -42,56 +41,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById("current-year").textContent = new Date().getFullYear();
 });
-
-/*function checkAuthentication() {
-    const token = localStorage.getItem('token'); // Recupera o token do localStorage
-    if (!token) {
-        // Se o token não existir, redireciona para a página de login
-        alert('Você precisa estar logado para acessar esta página.');
-        window.location.href = '../index.html';
-    } else {
-        // Valide o token no backend para garantir que ele é válido
-        fetch('https://portal-x09e.onrender.com/auth/validate-token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Token inválido ou expirado');
-                }
-            })
-            .catch(() => {
-                alert('Sessão expirada. Faça login novamente.');
-                localStorage.removeItem('token'); // Remove o token inválido
-                window.location.href = '../index.html';
-            });
-    }
-}
-
-// Chame a função em todas as páginas protegidas
-checkAuthentication();
-
-let logoutTimer;
-
-// Função para deslogar o usuário
-function logoutUser() {
-    alert('Você foi deslogado devido à inatividade.');
-    localStorage.removeItem('token'); // Remove o token do localStorage
-    window.location.href = '../index.html'; // Redireciona para a página de login
-}
-
-// Função para resetar o timer de inatividade
-function resetLogoutTimer() {
-    clearTimeout(logoutTimer);
-    logoutTimer = setTimeout(logoutUser, 15 * 60 * 1000); // 15 minutos de inatividade
-}
-
-// Reseta o timer em eventos de interação do usuário
-window.onload = resetLogoutTimer;
-document.onmousemove = resetLogoutTimer;
-document.onkeypress = resetLogoutTimer;
-document.onscroll = resetLogoutTimer;
-*/
